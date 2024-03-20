f450a54bf245e2b6c02e5fe8928d49f3c3e62b9d
Matthew Ruiz <mrr171@txstate.edu>
2024-03-19 20:50:49 -0500

    Basic obstacle avoidance with Motors and Sensors class calls.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,26 @@
+#include "Sensors.h"
+#include "Motors.h"
+
 #include <Arduino.h>
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
+    // put your setup code here, to run once:
+    Serial.begin(9600);
 }
 
+bool atGoal;
+
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println("Hello, world!");
+    // put your main code here, to run repeatedly:
+    while (!atGoal) {
+        if (!Sensors::wallLeft()) {
+            Motors::turnLeft();
+        }
+
+        while (Sensors::wallFront()) {
+            Motors::turnRight();
+        }
+
+        Motors::moveForward();
+    } // end while
 }
